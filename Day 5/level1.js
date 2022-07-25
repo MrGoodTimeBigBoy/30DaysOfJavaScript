0607e32ec45713ac42aec77155168b30c27c9a42
MrGoodTimeBigBoy <86003900+MrGoodTimeBigBoy@users.noreply.github.com>
2022-07-25 09:44:15 -0400

    day 5

---
+++ Day 5/level1.js
@@ -0,0 +1,34 @@
+//1.
+const empty = Array()
+console.log(empty)
+
+//2.
+const hasEight = Array(8)
+console.log(hasEight)
+
+//3.
+console.log(hasEight.length)
+
+//4.
+const nineWorlds = ['asgard', 'vanaheim', 'alfheim', 'midgard', 'jotunheim', 'muspellsheim', 'svartalfaheim', 'nilfheim', 'nidavellir']
+console.log(`First world: ${nineWorlds[0]}`)
+console.log(`Middle world: ${nineWorlds[4]}`)
+console.log(`Last world: ${nineWorlds[8]}`)
+
+//5. 
+let mixedDataTypes = [3, true, 'hello', NaN, 3.14, ,"bye"]
+console.log(mixedDataTypes.length)
+
+//6.
+let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+//7.
+console.log(itCompanies)
+
+//8.
+console.log(itCompanies.length)
+
+//9.
+console.log(`first IT Company: ${itCompanies[0]}`)
+console.log(`middle IT Company: ${itCompanies[3]}`)
+console.log(`last IT Company: ${itCompanies[6]}`)
