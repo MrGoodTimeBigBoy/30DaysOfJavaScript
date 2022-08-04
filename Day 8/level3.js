cac2802f98206a3975a4da01cf2837a696bcdca5
MrGoodTimeBigBoy <86003900+MrGoodTimeBigBoy@users.noreply.github.com>
2022-08-04 09:46:01 -0400

    Update level3.js

--- Day 8/level3.js
+++ Day 8/level3.js
@@ -15,7 +15,34 @@ const personAccount = {
             sumIncome+=source
         }
         return sumIncome
+    },
+    totalExpense: function(){
+        let sumExpense = 0
+        for (source of this.expenses){
+            sumExpense+=source
+        }
+        return sumExpense
+    },
+    accountInfo: function(){
+        return `This account belongs to ${this.firstName} ${this.lastName}.`
+    },
+    addIncome: function(amount){
+        this.incomes.push(amount)
+    },
+    addExpense: function(amount){
+        this.expenses.push(amount)
+    },
+    accountBalance: function(){
+        return this.totalIncome()-this.totalExpense()
     }
 }
 
 console.log(personAccount.totalIncome())
+console.log(personAccount.totalExpense())
+console.log(personAccount.accountInfo())
+console.log(personAccount.accountBalance())
+personAccount.addExpense(400)
+console.log(personAccount.accountBalance())
+
+//2
+
