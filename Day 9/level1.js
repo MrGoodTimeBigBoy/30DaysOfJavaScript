d5d0d053ce90378786d136d10af939302b7097b8
MrGoodTimeBigBoy <86003900+MrGoodTimeBigBoy@users.noreply.github.com>
2022-08-08 14:32:55 -0400

    DAY 9

--- Day 9/level1.js
+++ Day 9/level1.js
@@ -7,7 +7,7 @@ const products = [
   { product: 'potato', price: ' ' },
   { product: 'avocado', price: 8 },
   { product: 'coffee', price: 10 },
-  { product: 'tea', price: '' },
+  { product: 'tea', price: '' }
 ]
 
 //1
--- Day 9/level2.js
+++ Day 9/level2.js
@@ -3,5 +3,23 @@ const priceSum = products.map((item) => item.price).filter((item) => typeof item
 console.log(priceSum)
 
 //2
-const priceSumReduce = products.reduce((acc,cur) => acc + (cur.price.parseInt || 0),0)
+const priceSumReduce = products.reduce((acc,cur) => acc + isPrice(cur.price),0)
 console.log(priceSumReduce)
+
+function isPrice(input){
+    let value = input
+    if (typeof input != 'number'){
+        value = 0
+    }
+    return value
+}
+
+//3
+
+function categorizeCountries(array){
+    const stanArray = array.filter((country) => country.name.endsWith('stan'))
+    return stanArray
+}
+
+console.log(categorizeCountries(countriesAll))
+
