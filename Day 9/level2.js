ac856a93a1db842c9b456e953c9e35d8f4bfb8d7
MrGoodTimeBigBoy <86003900+MrGoodTimeBigBoy@users.noreply.github.com>
2022-08-09 10:03:48 -0400

    Update level2.js

--- Day 9/level2.js
+++ Day 9/level2.js
@@ -23,3 +23,41 @@ function categorizeCountries(array){
 
 console.log(categorizeCountries(countriesAll))
 
+//4
+const letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+const letterCountries = letters.map((letter) => createObject(letter))
+
+function createObject(input){
+    const count = countriesAll.filter((country)=> country.name.startsWith(input))
+    const outputObject = {
+        Letter: input,
+        Occurances: count.length
+    }
+    return outputObject
+}
+
+console.log(letterCountries)
+
+//5
+
+function getFirstTenCountries (array){
+    const outputArray = []
+    for (i=0; i<10; i++){
+        outputArray.push(array[i])
+    }
+    return outputArray
+}
+
+console.log(getFirstTenCountries(countriesAll))
+
+//6
+
+function getLastTenCountries (array){
+    const outputArray = []
+    for (i=10; i>0; i--){
+        outputArray.push(array[array.length-i])
+    }
+    return outputArray
+}
+
+console.log(getLastTenCountries(countriesAll))
