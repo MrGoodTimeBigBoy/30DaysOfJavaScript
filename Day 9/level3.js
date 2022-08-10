52664bdb328d69c67eaabc212c80b52f72ae80fe
MrGoodTimeBigBoy <86003900+MrGoodTimeBigBoy@users.noreply.github.com>
2022-08-10 09:16:00 -0400

    Update level3.js

--- Day 9/level3.js
+++ Day 9/level3.js
@@ -66,3 +66,28 @@ function mostPopulatedCountries(array, num){
 }
 
 console.log(mostPopulatedCountries(countriesAll,10))
+
+//4
+const ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
+
+const statistics = {
+    count: function() {
+        return ages.length
+    },
+    sum: function() {
+        return ages.reduce((acc,cur) => acc+ cur, 0)
+    }
+}
+
+console.log('Count:', statistics.count()) // 25
+console.log('Sum: ', statistics.sum()) // 744
+//console.log('Min: ', statistics.min()) // 24
+//console.log('Max: ', statistics.max()) // 38
+//console.log('Range: ', statistics.range() // 14
+//console.log('Mean: ', statistics.mean()) // 30
+//console.log('Median: ',statistics.median()) // 29
+//console.log('Mode: ', statistics.mode()) // {'mode': 26, 'count': 5}
+//console.log('Variance: ',statistics.var()) // 17.5
+//console.log('Standard Deviation: ', statistics.std()) // 4.2
+//console.log('Variance: ',statistics.var()) // 17.5
+//console.log('Frequency Distribution: ',statistics.freqDist()) # [(20.0, 26), (16.0, 27), (12.0, 32), (8.0, 37), (8.0, 34), (8.0, 33), (8.0, 31), (8.0, 24), (4.0, 38), (4.0, 29), (4.0, 25)]
